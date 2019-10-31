09866c08721612194e0f60b8b1ed86852312296a
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-10-31 23:28:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,15 +70,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.each do |season, holiday|
-    holiday.each do |stuff|
-      if stuff.include?("BBQ") == true 
-        then puts [holiday , stuff]
-      end
-      end
-    end
-  
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
+      holiday if item.include?("BBQ")
+    end 
+  end.flatten.compact
 end
+  
+
 
 
 
